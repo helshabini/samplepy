79d8169d3335bd465a549aa7eed406da32098d52
Hazem Elshabini <haelshab@microsoft.com>
2020-04-18 00:12:21 +0300

    Commit

--- app/app.py
+++ app/app.py
@@ -1,22 +1,18 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
-def helloworld():
+def hello():
     return 'Hello, World!'
 
 @app.route('/json')
 def json():
-   return """
-{
-    values: [{
-        value1,
-        value2,
-        value3
-    }]
-}
-   """
+    return '{param1: "value1", param2: "value2"}'
 
 if __name__ == '__main__':
-   app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
